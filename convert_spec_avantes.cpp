b2702e0b1599edfdaa19038e62866eb55ec97899
burm.leonid@gmail.com <burmist-git>
2023-05-02 15:36:32 +0200

    add convertion

--- convert_spec_avantes.cpp
+++ convert_spec_avantes.cpp
@@ -64,21 +64,89 @@ int main(int argc, char *argv[]){
       gr_v.at(i)->Write();
       std::cout<<i<<" "<<gr_v.at(i)->GetTitle()<<std::endl;
     }
-    //0.4mm
+    //////////////////////////////////////////
+    //
+    //00 ../data/30.04.2023//background.TXT gr_background_TXT
+    //01 ../data/30.04.2023//candle.TXT gr_candle_TXT
+    //02 ../data/30.04.2023//lamp.TXT gr_lamp_TXT
+    //03 ../data/30.04.2023//sample01.TXT gr_sample01_TXT
+    //04 ../data/30.04.2023//sample02.TXT gr_sample02_TXT
+    //05 ../data/30.04.2023//sample03.TXT gr_sample03_TXT
+    //06 ../data/30.04.2023//sample04.TXT gr_sample04_TXT
+    //07 ../data/30.04.2023//sample05.TXT gr_sample05_TXT
+    //08 ../data/30.04.2023//sample06.TXT gr_sample06_TXT
+    //09 ../data/30.04.2023//sample07_plastick_bag.TXT gr_sample07_plastick_bag_TXT
+    //10 ../data/30.04.2023//sample08_red_plastick_bag.TXT gr_sample08_red_plastick_bag_TXT
+    //11 ../data/30.04.2023//sample09_glass.TXT gr_sample09_glass_TXT
+    //12 ../data/30.04.2023//sample10_glass_watter.TXT gr_sample10_glass_watter_TXT
+    //13 ../data/30.04.2023//wood.TXT gr_wood_TXT
+    //
+    //////////////////////////////////////////
     TGraphErrors *gr_r01 = new TGraphErrors();
-    gr_r01->SetNameTitle("gr_0_4mm_resin_R","gr_0_4mm_resin_R");
-    get_ration(gr_r01, gr_v.at(0),gr_v.at(1));
+    gr_r01->SetNameTitle("gr_rNNL","gr_rNNL");
+    get_ration(gr_r01, gr_v.at(3),gr_v.at(2));
     gr_r01->Write();
-    //1.12mm
+    //
     TGraphErrors *gr_r02 = new TGraphErrors();
-    gr_r02->SetNameTitle("gr_1_12mm_resin_R","gr_1_12mm_resin_R");
-    get_ration(gr_r02, gr_v.at(2),gr_v.at(3));
+    gr_r02->SetNameTitle("gr_rNaOchi","gr_rNaOchi");
+    get_ration(gr_r02, gr_v.at(4),gr_v.at(2));
     gr_r02->Write();
-    //1.4mm
+    //
     TGraphErrors *gr_r03 = new TGraphErrors();
-    gr_r03->SetNameTitle("gr_1_4mm_resin_R","gr_1_4mm_resin_R");
-    get_ration(gr_r03, gr_v.at(4),gr_v.at(5));
+    gr_r03->SetNameTitle("gr_rNNA","gr_rNNA");
+    get_ration(gr_r03, gr_v.at(5),gr_v.at(2));
     gr_r03->Write();
+    //
+    TGraphErrors *gr_r04 = new TGraphErrors();
+    gr_r04->SetNameTitle("gr_rPolaroid","gr_rPolaroid");
+    get_ration(gr_r04, gr_v.at(6),gr_v.at(2));
+    gr_r04->Write();
+    //
+    TGraphErrors *gr_r05 = new TGraphErrors();
+    gr_r05->SetNameTitle("gr_rRayBan","gr_rRayBan");
+    get_ration(gr_r05, gr_v.at(7),gr_v.at(2));
+    gr_r05->Write();
+    //
+    TGraphErrors *gr_r06 = new TGraphErrors();
+    gr_r06->SetNameTitle("gr_rLAB","gr_rLAB");
+    get_ration(gr_r06, gr_v.at(8),gr_v.at(2));
+    gr_r06->Write();
+    //
+    TGraphErrors *gr_r07 = new TGraphErrors();
+    gr_r07->SetNameTitle("gr_rPlasticbag","gr_rPlasticbag");
+    get_ration(gr_r07, gr_v.at(9),gr_v.at(2));
+    gr_r07->Write();
+    //
+    TGraphErrors *gr_r08 = new TGraphErrors();
+    gr_r08->SetNameTitle("gr_rRedplasticbag","gr_rRedplasticbag");
+    get_ration(gr_r08, gr_v.at(10),gr_v.at(2));
+    gr_r08->Write();
+    //
+    TGraphErrors *gr_r09 = new TGraphErrors();
+    gr_r09->SetNameTitle("gr_rGlass","gr_rGlass");
+    get_ration(gr_r09, gr_v.at(11),gr_v.at(2));
+    gr_r09->Write();
+    //
+    TGraphErrors *gr_r10 = new TGraphErrors();
+    gr_r10->SetNameTitle("gr_rGlasswithwater","gr_rGlasswithwater");
+    get_ration(gr_r10, gr_v.at(12),gr_v.at(2));
+    gr_r10->Write();
+    //
+    //0.4mm
+    //TGraphErrors *gr_r01 = new TGraphErrors();
+    //gr_r01->SetNameTitle("gr_0_4mm_resin_R","gr_0_4mm_resin_R");
+    //get_ration(gr_r01, gr_v.at(0),gr_v.at(1));
+    //gr_r01->Write();
+    //1.12mm
+    //TGraphErrors *gr_r02 = new TGraphErrors();
+    //gr_r02->SetNameTitle("gr_1_12mm_resin_R","gr_1_12mm_resin_R");
+    //get_ration(gr_r02, gr_v.at(2),gr_v.at(3));
+    //gr_r02->Write();
+    //1.4mm
+    //TGraphErrors *gr_r03 = new TGraphErrors();
+    //gr_r03->SetNameTitle("gr_1_4mm_resin_R","gr_1_4mm_resin_R");
+    //get_ration(gr_r03, gr_v.at(4),gr_v.at(5));
+    //gr_r03->Write();
     //
     rootFile->Close();
     std::cout<<"  Output TGrpah file ---> "<<outroot.Data()<<std::endl;
@@ -95,6 +163,17 @@ int main(int argc, char *argv[]){
   //
   return 0;
 }
+
+
+//Integration time [ms]:  10.000
+//Averaging Nr. [scans]: 10
+//Smoothing Nr. [pixels]: 1
+//Data measured with spectrometer [name]: 1808445U1
+//Wave   Sample   Dark     ReferenceScope
+//[nm]   [counts] [counts] [counts] 
+//
+//176.578    3.288  496.001  496.001
+
 
 //0.4mm
 //Integration time [ms]:   1.000
@@ -153,7 +232,8 @@ void get_ration(TGraphErrors *gr_r, TGraphErrors *gr, TGraphErrors *gr_n){
     an_err = get_sample_err(an);
     if(an>0.0 && a>0.0){
       r = a/an;
-      r_err = r*TMath::Sqrt(a_err/a*a_err/a + an_err/an*an_err/an);
+      //r_err = r*TMath::Sqrt(a_err/a*a_err/a + an_err/an*an_err/an);
+      r_err = 0.0;
       gr_r->SetPoint( nr, wl, r);
       gr_r->SetPointError( nr, wl_err, r_err);
       nr++;
@@ -162,9 +242,11 @@ void get_ration(TGraphErrors *gr_r, TGraphErrors *gr, TGraphErrors *gr_n){
 }
 
 Double_t get_wl_err(Double_t wl){
-  return 0.1;
+  //return 0.1;
+  return 0.01;
 }
 
 Double_t get_sample_err(Double_t a){
-  return a/10.0 + 10.0;
+  //return a/10.0 + 10.0;
+  return 1.0;
 }
--- convert_spec_avantes.sh
+++ convert_spec_avantes.sh
@@ -3,7 +3,9 @@
 function printHelp {
     echo " --> ERROR in input arguments "
     echo " [0] -d    : default"
-    echo " [0] -info : gen info file"
+    echo " [0] -30.04.2023         : convert 30.04.2023"
+    echo " [0] -info_17.03.2023_02 : gen info file for (17.03.2023_02)"
+    echo " [0] -info_30.04.2023    : gen info file for (30.04.2023)"
     echo " [0] -h    : print help"
 }
 
@@ -30,8 +32,17 @@ else
 	info_file="../data/17.03.2023_02/info"
 	out_root="../data/17.03.2023_02/hist.root"
 	./convert_spec_avantes 0 $info_file $out_root | tee -a $convertlog
-    elif [ "$1" = "-info" ]; then
+    elif [ "$1" = "-30.04.2023" ]; then
+	make clean; make;
+	convertlog="../data/30.04.2023/convert_spec_avantes.log"
+	rm -rf $convertlog
+	info_file="../data/30.04.2023/info"
+	out_root="../data/30.04.2023/hist.root"
+	./convert_spec_avantes 0 $info_file $out_root | tee -a $convertlog
+    elif [ "$1" = "-info_17.03.2023_02" ]; then
 	gen_info "../data/17.03.2023_02/"
+    elif [ "$1" = "-info_30.04.2023" ]; then
+	gen_info "../data/30.04.2023/"
     elif [ "$1" = "-h" ]; then
         printHelp
     else
